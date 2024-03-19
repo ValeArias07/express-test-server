4b747da00bf2bd025f1e4be372aff2006c999cbd
Valentina Arias Parra <54784992+ValeArias07@users.noreply.github.com>
2024-03-19 11:36:28 -0500

    add the server file

---
+++ server.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const path = require('path');
+
+const app = express();
+const PORT = process.env.PORT || 80;
+
+// Configurar Express para servir archivos estáticos
+app.use(express.static(path.join(__dirname, 'public')));
+
+// Ruta para servir el archivo HTML
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public', 'index.html'));
+});
+
+// Ruta para servir la imagen
+app.get('/ver-imagen/:name', (req, res) => {
+  const name = req.params.name; 
+  res.sendFile(path.join(__dirname, 'public', `${name}.jpg`));
+});
+
+// Iniciar el servidor
+app.listen(PORT, () => {
+  console.log(`Servidor corriendo en el puerto ${PORT}`);
+});
